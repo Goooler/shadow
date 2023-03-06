ebb2cd77091fd6fcd893d1c44fb06be4eebb7e17
Goooler <wangzongler@gmail.com>
2023-03-06 10:21:01 +0800

    Cleanup

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.groovy
@@ -4,21 +4,22 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.SelfResolvingDependency
+import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Provider
 import org.gradle.api.publish.maven.MavenPom
 import org.gradle.api.publish.maven.MavenPublication
 
 class ShadowExtension {
-    private final def archiveFile
+    private final Provider<Provider<RegularFile>> archiveFile
     private final Provider<List<Dep>> allDependencies
 
     ShadowExtension(Project project) {
         archiveFile = project.provider { project.tasks.withType(ShadowJar).getByName("shadowJar").archiveFile }
         allDependencies = project.provider {
-            project.configurations.getByName("shadow").allDependencies.findAll {
-                it instanceof ProjectDependency || it instanceof SelfResolvingDependency
-            }.collect {
-                new Dep(it.group, it.name, it.version)
+            project.configurations.getByName("shadow").allDependencies.collect {
+                if ((it instanceof ProjectDependency) || !(it instanceof SelfResolvingDependency)) {
+                    new Dep(it.group, it.name, it.version)
+                }
             }
         }
     }
@@ -26,11 +27,11 @@ class ShadowExtension {
     void component(MavenPublication publication) {
         publication.artifact(archiveFile.get())
 
-        final def allDeps = allDependencies.get()
+        final def allDeps = allDependencies
         publication.pom { MavenPom pom ->
             pom.withXml { xml ->
                 def dependenciesNode = xml.asNode().get('dependencies') ?: xml.asNode().appendNode('dependencies')
-                allDeps.each {
+                allDeps.get().each {
                     def dependencyNode = dependenciesNode.appendNode('dependency')
                     dependencyNode.appendNode('groupId', it.group)
                     dependencyNode.appendNode('artifactId', it.name)
